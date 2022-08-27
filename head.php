4b583bcb0c58a980da11f709c6bb9fc7c131a2cd
Adramaryl <alice.lacroix.95@gmail.com>
2022-08-27 11:56:55 +0200

    separated head from rest of the page

---
+++ head.php
@@ -0,0 +1,26 @@
+<?php $appName = "Spinnin' Events"; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?=$appName ?><?= isset($title) ? " - $title" : "" ?></title>
+    <meta name="description" content="<?= $site_description ?>">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="<?= $appName ?><?= isset($title) ? " - $title" : "" ?>">
+    <meta property="og:site_name" content="<?= $appName ?>" />
+    <?php if(isset($site_description)): ?>
+        <meta property="og:description" content="<?= $site_description ?>">
+    <?php endif; ?>
+    <meta property="og:image" content="https://devenir.dj/wp-content/uploads/2015/08/spinnin_records_wallpaper__normal__by_angiegehtsteil-d8ic15b.png">
+
+    <meta name="twitter:site" content="<? $appName ?>">
+    <meta name="twitter:card" content="summary">
+
+    <meta name="keywords" content="Music, Events, Records, Spinnin">
+
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+</head>
--- view.php
+++ view.php
@@ -1,21 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Spinnin' Events</title>
-
-        <meta name="keywords" content="Music, Events, Records, Spinnin">
-
-        <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
+    <?php 
+        $title = "Upcoming Events";
+        $site_description = "Our upcoming events";
+        require "head.php"; 
+    ?>
     <body>
         <header>
+            <a href="index.php"><img src="images/spinnin_logo.png"></a>
             <nav>
-                <a href="index.php"><img src="images/spinnin_logo.png">&nbsp;<span class="bold">Spinnin'</span> <span class="thin">Events</span></a>
+                <a href="index.php"><span class="bold">Spinnin'</span> <span class="thin">Events</span></a>
                 <a href="https://spinninrecords.com/"><span class="bold">Spinnin'</span> <span class="thin">Records</span></a>
                 <a href="" class="login">Sign up | Log in</a>
             </nav>
@@ -31,11 +25,13 @@
 
             <div class="events">
                 <?php foreach($events_list as $event): ?>
-                    <div>
-                        <img src="<?= $event["image"] ?>" alt="">
-                        <h2><?= $event["title"] ?></h2>
-                        <time><?= date_format($event["date"], "d/m/Y")?></time>
-                    </div>
+                    <a href="">
+                        <div class="elevent">
+                            <img src="<?= $event["image"] ?>" alt="">
+                            <h2><?= $event["title"] ?></h2>
+                            <time><?= date_format($event["date"], "d/m/Y")?></time>
+                        </div>
+                    </a>
                 <?php endforeach; ?>
             </div>
         </main>
