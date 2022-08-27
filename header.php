6573827d46fb496a722b37cd461f0ca882c0d071
Adramaryl <alice.lacroix.95@gmail.com>
2022-08-28 01:00:05 +0200

    hotfix header link

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header>
     <a href="index.php"><img src="/spinnin_events/images/spinnin_logo.png"></a>
     <nav>
-        <a href="index.php"><span class="bold">Spinnin'</span> <span class="thin">Events</span></a>
+        <a href="/spinnin_events/index.php"><span class="bold">Spinnin'</span> <span class="thin">Events</span></a>
         <a href="https://spinninrecords.com/"><span class="bold">Spinnin'</span> <span class="thin">Records</span></a>
-        <a href="" class="login">Sign up | Log in</a>
+        <a href="/spinnin_events/login" class="login">Sign up | Log in</a>
     </nav>
 </header>
