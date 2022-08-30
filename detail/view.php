4d4e12cbdb8004fdc5ba7f21d0434c72bc93ae08
Adramaryl <alice.lacroix.95@gmail.com>
2022-08-30 15:03:05 +0200

    minor edits

--- detail/view.php
+++ detail/view.php
@@ -3,10 +3,10 @@
     <?php 
         $title = "Event detail";
         $site_description = "More info on this event";
-        require "head.php"; 
+        require "../head.php"; 
     ?>
     <body>
-        <?php require "header.php"; ?>
+        <?php require "../header.php"; ?>
         <main>
             <?php foreach($details_list as $detail): ?>
 
@@ -15,17 +15,30 @@
             </div>
 
             <div class="detail">
-                <img src="<?= $detail["image"] ?>" alt="">
-                <video width="320" height="240" controls>
-                    <source src="<?= $detail["video_link"] ?>">
-                </video>
-                <time><?= date_format($detail["date"], "d/m/Y")?></time>
-                <p><?= $detail["info"] ?></p>
-                <a href="<?= $detail["tickets_link"] ?>">Buy tickets</a>
+                <div>
+                    <img src="../<?= $detail["image"] ?>" alt="">
+                </div>
+
+                <div>
+                    <iframe width="500" height="300" src="<?= $detail["video_link"] ?>?controls">
+                    </iframe>
+                </div>
+
+                <div>
+                    <time><?= date_format($detail["date"], "d/m/Y")?></time>
+                </div>
+
+                <div>
+                    <p><?= $detail["info"] ?></p>
+                </div>
+
+                <div>
+                    <button><a href="<?= $detail["tickets_link"] ?>">Buy tickets</a></button>
+                </div>
             </div>
 
             <?php endforeach; ?>
         </main>
-        <?php require "footer.php"; ?>
+        <?php require "../footer.php"; ?>
     </body>
 </html>
